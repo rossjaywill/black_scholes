a1b3f34893dc97ac4c3b3a08fdd063e0053f05a4
Ross Williamson <rjw@pop-os.localdomain>
2022-06-12 20:01:40 +0100

    WIP: In progress arg parse; update CXX standard

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <string_view>
-#include <vector>
 #include <fmt/core.h>
 
 #include "black_scholes.h"
@@ -13,13 +13,13 @@ using namespace black_scholes;
 void displayHelp()
 {
     fmt::print("Direct run of BSM command require five arguments:\n"
-                "-o | --option-type          : Type of option ('call' or 'put')\n"
-                "-u | --underling-price      : Price of underlying asset\n"
-                "-s | --strike-price         : Strike price of the options contract\n"
-                "-v | --implied-volatility   : Implied volatility of underlying asset\n"
-                "-r | --interest-rate        : Risk Free interest rate\n"
-                "-t | --time-to-expiry       : Time to next (front) expiry date of the option contract in days\n\n"
-                "(-h | --help                 : Display this help/usage message)\n"
+                "\t-o | --option-type          : Type of option ('call' or 'put')\n"
+                "\t-u | --underling-price      : Price of underlying asset\n"
+                "\t-s | --strike-price         : Strike price of the options contract\n"
+                "\t-v | --implied-volatility   : Implied volatility of underlying asset\n"
+                "\t-r | --interest-rate        : Risk Free interest rate\n"
+                "\t-t | --time-to-expiry       : Time to next (front) expiry date of the option contract in days\n\n"
+                "\t(-h | --help                 : Display this help/usage message)\n"
                 "Optionally, if implied-volatility and interest-rate are omitted, the program will use "
                 "default assumptions for these values.\n");
 }
@@ -32,24 +32,40 @@ void displayHelp()
 
 void parseArgs(int argc, char **argv)
 {
-    const std::vector<std::string_view> params { argv + 1, argv + argc };
+    const std::set<std::string_view> baseOpts {
+        "-o", "-u", "-s", "-t",
+        "--option-type", "--underlying-price", "--strike-price", "--time-to-expiry"
+    };
+    const std::vector<std::string_view> addOpts = {
+        "-v", "-r", "--interest-rate", "--time-to-expiry"
+    };
+    std::set<std::string_view> fullOpts = baseOpts;
+    for (const auto &opt : addOpts) {
+        fullOpts.insert(opt);
+    }
 
-    if (params.size() == 1) {
-        if (params[0] == "-h" || params[0] == "--help") {
-            helpAndExit(0);
-        }
-        helpAndExit(1);
+    const std::set<std::string_view> params { argv + 1, argv + argc };
+
+    auto within = [&fullOpts](std::string_view arg) { return fullOpts.find(arg) != fullOpts.end(); };
+
+    if (std::any_of(params.begin(), params.end(), [](std::string_view arg) {
+                return (arg == "-h" || arg == "--help");
+            })) {
+        helpAndExit(0);
     }
-    else if (params.size() == 3) {
-        // run with assumptions
-        BlackScholes<CallPutFlag> bsm;
+    else if (params.size() == 4 &&
+            std::all_of(params.begin(), params.end(), within)) {
+        fmt::print("All 3 found");
     }
-    else if (params.size() == 5) {
-        // run without assumptions
+    else if (params.size() == 6 &&
+            std::all_of(params.begin(), params.end(), within)) {
+        fmt::print("All 5 found");
     }
     else {
         helpAndExit(1);
     }
+
+
 }
 
 int main(int argc, char **argv)
